9eae6a1fb3cb114f65ebb9bfc26c7a268e98f26e
Nissi Tafie <ntafie@blockdaemon.com>
2022-04-23 11:50:25 +0100

    k0s

--- k8s/k0s/main.go
+++ k8s/k0s/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	out, err := exec.Command("curl",  "https://get.k0s.sh", "-sSLf", "|", "sudo", "sh").Output()
+	curlOut := exec.Command("curl", "https://get.k0s.sh", "-sSLf")
+	sudoCmd := exec.Command("sudo", "sh")
 
+	r, w := io.Pipe()
+
+	curlOut.Stdout = w
+	sudoCmd.Stdin = r
+
+	var b2 bytes.Buffer
+	sudoCmd.Stdout = &b2
+
+	err := curlOut.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sudoCmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = curlOut.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = w.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sudoCmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	wr, err := io.Copy(os.Stdout, &b2)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(out))
+	log.Println(wr)
+
 }
